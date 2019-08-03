8def04b9365f61dcaddf01487dfab33cf03590ce
sarantejavvds <52404660+sarantejavvds@users.noreply.github.com>
2019-08-03 13:31:34 +0530

    Add files via upload

---
+++ saran.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int i;
+for(i=0;i<=10;i++)
+{
+printf("saranteja\n");
+}
+i=1;
+while(i<=10)
+{
+printf("saranteja\n");
+i++;
+}
+i=1;
+do
+{
+printf("saranteja\n");
+i++;
+}while(i<=10);
+}
