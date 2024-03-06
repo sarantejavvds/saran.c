06938868179209a684bd2ebae81a8521293771c1
VELUGUBANTLA VENKATA DURGA SAI SARAN TEJA <52404660+sarantejavvds@users.noreply.github.com>
2024-03-06 23:26:53 +0530

    Update saran_for_while_dowhile_Loops.c

--- saran.c
+++ saran.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int i;
-for(i=0;i<=10;i++)
-{
-printf("saranteja\n");
-}
-i=1;
-while(i<=10)
-{
-printf("saranteja\n");
-i++;
-}
+ int i;
+ for(i=0;i<=10;i++)
+ {
+  printf("saranteja\n");
+ }
+  
+ i=1;
+ while(i<=10)
+ {
+  printf("saranteja\n");
+  i++;
+ }
+  
 i=1;
 do
 {
-printf("saranteja\n");
-i++;
-}while(i<=10);
+ printf("saranteja\n");
+ i++;
+ }while(i<=10);
+
+ return 0;
 }
